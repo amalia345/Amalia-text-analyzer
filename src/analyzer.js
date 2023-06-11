0a5acb9b4e8bc6a6a6e892e1c6e6d62b8f4b3cd6
amalia345 <sotoes.amalia@gmail.com>
2023-06-11 15:29:23 -0600

    Termine funciones JS

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,26 +1,49 @@
 const analyzer = {  
-  getWordCount: (text) => {
-    return text
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getWordCount: (text) => {// utilce split para crear una variable que separa con espacio (' ') para crear un arreglo de strings y darle.lenght
+    let palabrasSeparadas = text.split(' ') //separa el string usando espacio como identificador 
+    return "Palabras :" + palabrasSeparadas.length;
   },
   getCharacterCount: (text) => {
-    return "Caracteres: " +text.length
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return "Caracteres: " + text.length
   },
   getCharacterCountExcludingSpaces: (text) => {
-    console.log(text)
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let cadenaSinEspacios = text.replace(/[^\w]/gi,"")//Expresion regular w elimina caracteres especiales y espacios, g de agarra todas, i agarra minusculas y mayosculas
+    return "Caracteres sin espacios: " + cadenaSinEspacios.length
   },
   getAverageWordLength: (text) => {    
-    console.log(text)
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    return  "Promedio Longitud: " + text.replace(/[^\w]/gi,"").length / text.split(' ').length
+
   },
   getNumberCount: (text) => {
-    console.log(text)
+    let listaPalabras = text.split(' ')
+    let listaNumeros = [];
+    for (let i = 0; i < listaPalabras.length; i++) {
+      if (listaPalabras[i].match(/[0-9]+/g)) { // SI el elemento i tiene un numero
+        listaNumeros.push(parseInt(listaPalabras[i]))// conviertelo a numero con parseint y guardalo en Listadepalabras
+      } else {
+        console.log('no es numero');
+      }
+      
+    }
+    return "Números: " + listaNumeros.length;
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
-    console.log(text)
+    let listaPalabras = text.split(' ')
+    let listaNumeros = [];
+
+    listaPalabras.forEach(element => {
+      if (element.match(/[0-9]+/g)) { // SI el elemento i tiene un numero
+        listaNumeros.push(parseInt(element))// conviertelo a numero con parseint y guardalo en Listadepalabras
+      }
+    });
+
+    let suma=0;
+
+    listaNumeros.forEach(element => {
+      suma+=element
+    });
+    return "Suma números: " + suma;
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import analyzer from './analyzer.js';
 console.log(analyzer)
-let cajitaText = document.querySelector("textarea[name='user-input']")
-let palabra = document.querySelector(".resultados li")
+const cajitaText = document.querySelector("textarea[name='user-input']")
+const listaMetricas = document.querySelectorAll(".resultados li")
 const eliminar = document.getElementById("reset-button")
 
 cajitaText.addEventListener('keyup',analizar)
 eliminar.addEventListener('click',refrescar)
 
 function analizar() {
-    const palabra1 = cajitaText.value
-    palabra.innerHTML=analyzer.getCharacterCount(palabra1)
+    let palabra1 = cajitaText.value
+    listaMetricas[0].innerHTML=analyzer.getWordCount(palabra1)
+    listaMetricas[1].innerHTML=analyzer.getCharacterCount(palabra1) //no mover este
+    listaMetricas[2].innerHTML=analyzer.getCharacterCountExcludingSpaces(palabra1)
+    listaMetricas[3].innerHTML=analyzer.getAverageWordLength(palabra1)
+    listaMetricas[4].innerHTML=analyzer.getNumberCount(palabra1)
+    listaMetricas[5].innerHTML=analyzer.getNumberSum(palabra1)
 }
 function refrescar() {
     cajitaText.value="" // agarramos el text area y le metemos unstring vacio
