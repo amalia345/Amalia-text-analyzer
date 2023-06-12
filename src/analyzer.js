a2aa7127e160ee733815eff74b9a08310f8a77dd
amalia345 <sotoes.amalia@gmail.com>
2023-06-12 12:35:52 -0600

    correcciones al test automatico

--- src/analyzer.js
+++ src/analyzer.js
@@ -11,19 +11,19 @@ const analyzer = {
     return "Caracteres sin espacios: " + cadenaSinEspacios.length
   },
   getAverageWordLength: (text) => {    
-    return  "Promedio Longitud: " + text.replace(/[^\w]/gi,"").length / text.split(' ').length
+    return  "Promedio Longitud: " +  (text.replace(/[^\w]/gi,"").length / text.split(' ').length).toFixed(2)
 
   },
   getNumberCount: (text) => {
     let listaPalabras = text.split(' ')
     let listaNumeros = [];
     for (let i = 0; i < listaPalabras.length; i++) {
-      if (listaPalabras[i].match(/[0-9]+/g)) { // SI el elemento i tiene un numero
-        listaNumeros.push(parseInt(listaPalabras[i]))// conviertelo a numero con parseint y guardalo en Listadepalabras
-      } else {
-        console.log('no es numero');
-      }
-      
+    let numeroPars=parseInt(listaPalabras[i])
+    if (!isNaN(numeroPars)) { // SI el elemento i tiene un numero
+    listaNumeros.push(numeroPars)// conviertelo a numero con parseint y guardalo en Listadepalabras
+    } else {
+    console.log('no es numero');
+    }
     }
     return "Números: " + listaNumeros.length;
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
@@ -31,17 +31,15 @@ const analyzer = {
   getNumberSum: (text) => {
     let listaPalabras = text.split(' ')
     let listaNumeros = [];
-
     listaPalabras.forEach(element => {
-      if (element.match(/[0-9]+/g)) { // SI el elemento i tiene un numero
-        listaNumeros.push(parseInt(element))// conviertelo a numero con parseint y guardalo en Listadepalabras
-      }
+    if (element.match(/[0-9]+/g)) { // SI el elemento i tiene un numero listaNumeros.push(parseInt(element))// conviertelo a numero con parseint y guardalo en Listadepalabras
+    }
     });
 
     let suma=0;
 
     listaNumeros.forEach(element => {
-      suma+=element
+    suma+=element
     });
     return "Suma números: " + suma;
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ eliminar.addEventListener('click',refrescar)
 
 function analizar() {
     let palabra1 = cajitaText.value
-    listaMetricas[0].innerHTML=analyzer.getWordCount(palabra1)
-    listaMetricas[1].innerHTML=analyzer.getCharacterCount(palabra1) //no mover este
-    listaMetricas[2].innerHTML=analyzer.getCharacterCountExcludingSpaces(palabra1)
-    listaMetricas[3].innerHTML=analyzer.getAverageWordLength(palabra1)
-    listaMetricas[4].innerHTML=analyzer.getNumberCount(palabra1)
-    listaMetricas[5].innerHTML=analyzer.getNumberSum(palabra1)
+    listaMetricas[2].innerHTML=analyzer.getWordCount(palabra1)
+    listaMetricas[0].innerHTML=analyzer.getCharacterCount(palabra1) //no mover este
+    listaMetricas[1].innerHTML=analyzer.getCharacterCountExcludingSpaces(palabra1)
+    listaMetricas[5].innerHTML=analyzer.getAverageWordLength(palabra1)
+    listaMetricas[3].innerHTML=analyzer.getNumberCount(palabra1)
+    listaMetricas[4].innerHTML=analyzer.getNumberSum(palabra1)
 }
 function refrescar() {
-    cajitaText.value="" // agarramos el text area y le metemos unstring vacio
+    cajitaText.value=" " // agarramos el text area y le metemos unstring vacio
     window.location.reload();// refresca la ventana
 }
