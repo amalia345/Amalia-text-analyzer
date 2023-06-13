5f2fc9d6cda3ceef40009ed36df8a42c7423a0cd
amalia345 <sotoes.amalia@gmail.com>
2023-06-13 01:05:04 -0600

    funciona todo menos dos pruebas

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,4 +1,4 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {// utilce split para crear una variable que separa con espacio (' ') para crear un arreglo de strings y darle.lenght
     let palabrasSeparadas = text.split(' ') //separa el string usando espacio como identificador 
     return "Palabras :" + palabrasSeparadas.length;
@@ -7,39 +7,42 @@ const analyzer = {
     return "Caracteres: " + text.length
   },
   getCharacterCountExcludingSpaces: (text) => {
-    let cadenaSinEspacios = text.replace(/[^\w]/gi,"")//Expresion regular w elimina caracteres especiales y espacios, g de agarra todas, i agarra minusculas y mayosculas
+    let cadenaSinEspacios = text.replace(/[^\w]/gi, "")//Expresion regular w elimina caracteres especiales y espacios, g de agarra todas, i agarra minusculas y mayosculas
     return "Caracteres sin espacios: " + cadenaSinEspacios.length
   },
-  getAverageWordLength: (text) => {    
-    return  "Promedio Longitud: " +  (text.replace(/[^\w]/gi,"").length / text.split(' ').length).toFixed(2)
-
+  getAverageWordLength: (text) => {
+    return (text.replace(/[^\w]/gi, "").length / text.split(' ').length).toFixed(2)
   },
+
   getNumberCount: (text) => {
     let listaPalabras = text.split(' ')
     let listaNumeros = [];
     for (let i = 0; i < listaPalabras.length; i++) {
-    let numeroPars=parseInt(listaPalabras[i])
-    if (!isNaN(numeroPars)) { // SI el elemento i tiene un numero
-    listaNumeros.push(numeroPars)// conviertelo a numero con parseint y guardalo en Listadepalabras
-    } else {
-    console.log('no es numero');
-    }
+      let numeroPars = parseInt(listaPalabras[i])
+      if (!isNaN(numeroPars)) { // SI el elemento i tiene un numero
+        listaNumeros.push(numeroPars)// conviertelo a numero con parseint y guardalo en Listadepalabras
+      } else {
+        console.log('no es numero');
+      }
     }
     return "Números: " + listaNumeros.length;
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+    console.log(text)
     let listaPalabras = text.split(' ')
     let listaNumeros = [];
-    listaPalabras.forEach(element => {
-    if (element.match(/[0-9]+/g)) { // SI el elemento i tiene un numero listaNumeros.push(parseInt(element))// conviertelo a numero con parseint y guardalo en Listadepalabras
-    }
+
+    listaPalabras.forEach(palabra => { //ciclo for/
+      if (!isNaN(palabra)) { // SI el elemento i tiene un numero
+        listaNumeros.push(parseFloat(palabra))// conviertelo a numero con parseint y guardalo en Listadepalabras
+      }// push es guardaar en unalista
     });
 
-    let suma=0;
+    let suma = 0;
 
-    listaNumeros.forEach(element => {
-    suma+=element
+    listaNumeros.forEach(numero => {
+      suma += numero
     });
     return "Suma números: " + suma;
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import analyzer from './analyzer.js';
-console.log(analyzer)
+
 const cajitaText = document.querySelector("textarea[name='user-input']")
 const listaMetricas = document.querySelectorAll(".resultados li")
 const eliminar = document.getElementById("reset-button")
+
+const promedioSpan = document.getElementById("promedio")
 
 cajitaText.addEventListener('keyup',analizar)
 eliminar.addEventListener('click',refrescar)
@@ -10,13 +12,13 @@ eliminar.addEventListener('click',refrescar)
 function analizar() {
     let palabra1 = cajitaText.value
     listaMetricas[2].innerHTML=analyzer.getWordCount(palabra1)
-    listaMetricas[0].innerHTML=analyzer.getCharacterCount(palabra1) //no mover este
+    listaMetricas[0].innerHTML=analyzer.getCharacterCount(palabra1) 
     listaMetricas[1].innerHTML=analyzer.getCharacterCountExcludingSpaces(palabra1)
-    listaMetricas[5].innerHTML=analyzer.getAverageWordLength(palabra1)
+    promedioSpan.innerHTML=analyzer.getAverageWordLength(palabra1)
     listaMetricas[3].innerHTML=analyzer.getNumberCount(palabra1)
     listaMetricas[4].innerHTML=analyzer.getNumberSum(palabra1)
 }
 function refrescar() {
-    cajitaText.value=" " // agarramos el text area y le metemos unstring vacio
-    window.location.reload();// refresca la ventana
+    cajitaText.value=""// agarramos el text area y le metemos unstring vacio
+  //  window.location.reload();// refresca la ventana // 
 }
